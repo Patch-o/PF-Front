f2700b5c3e99aa0e5a7499733407c5eae33e0d7a
Gonzalo <gonzalo.estebarena@bootcamp-upgrade.com>
2022-09-12 19:27:25 +0200

    Carrousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
+import ControlledCarousel from "../components/Carrrousel";
 import LibraryPost from "../components/Librarys/LibraryPost";
 import home from "./home.scss";
+
+
+
+
 
 const Home = () => {
   return (
     <div className="cards">
       <h1>POST</h1>
       {/* <div><LibraryPost /></div> */}
+      <ControlledCarousel/>
     </div>
   )
 };
