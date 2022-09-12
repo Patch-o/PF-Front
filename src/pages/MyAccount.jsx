b1cbfcb57be1766e890e15e1564111f784c4f1a9
JoanneZamorano <joannezamorano@gmail.com>
2022-09-12 10:55:35 +0200

    FEAT: pintar datos de perfil

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,16 +1,29 @@
 
 //import FormPost from "../components/Forms/FormPost";
+import { useSelector } from "react-redux";
 import Axios from "../components/Forms/Axios";
 import FormPostC from "../components/Forms/FormPostC";
 import FormProfile from "../components/Forms/FormProfile";
 
 
 const MyAccount = () => {
-  
+  const { user } = useSelector(state => state.auth);
   return (
     <div className="cardsProfile">
     <Axios/>
-      <div className="profileForm"><FormProfile /></div>
+      <div className="profileForm">
+      {user && <h3>Nombre: {user.name}</h3>}
+      {user && <h3>Email: {user.email}</h3>}
+      {user && <h3>id: {user._id}</h3>}
+      {user && <h3>Edad: {user.age}</h3>}
+      {user && <h3>Linkedin: {user.Linkedin}</h3>}
+      {user && <h3>Git: {user.gitplatform}</h3>}
+      {user && <h3>Vercel: {user.Vercel}</h3>}
+
+
+
+      <FormProfile />
+      </div>
 
       <div className="postForm"><FormPostC /> </div>
 
