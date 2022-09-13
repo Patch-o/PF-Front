1615b007210fb8dea4b5b9a2ff3c00893537b8c6
JoanneZamorano <joannezamorano@gmail.com>
2022-09-13 10:52:16 +0200

    FEAT: contenido y css del carrusel y FAQ

--- src/components/Carrrousel.jsx
+++ src/components/Carrrousel.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Carrusel.scss";
+
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -13,42 +15,54 @@ function ControlledCarousel() {
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          className="item__img"
+          src="https://cdn.discordapp.com/attachments/1014921169595531369/1018924298901131345/myAvatar.png"
           alt="First slide"
         />
         <Carousel.Caption >
-        <div className="d-block w-100">
-          <h1>Nombre</h1>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        <div className="text__content">
+          <h1>Boris</h1>
+          <p>Comida favorita: </p>
+          <p>Serie que veríamos 1000 veces: </p>
+          <p>Un lugar en el mundo: Roma</p>
+          <p>Canción que cantas en la ducha: </p>
+          <p>Pizza con o sin piña: </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="item__img"
+          src="https://cdn.discordapp.com/attachments/1014921169595531369/1018910916802265088/myAvatar.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-        <div className="d-block w-100">
-          <h1>Nombre</h1>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        <div className="text__content">
+          <h1>Joanne</h1>
+          <p>Comida favorita:  tarta de galletas de la abuela</p>
+          <p>Serie que veríamos 1000 veces: Como conocí a vuestra madre / New Girl</p>
+          <p>Un lugar en el mundo: Roma</p>
+          <p>Canción que cantas en la ducha: coldplay - viva la vida</p>
+          <p>Pizza con o sin piña: ¡SIN!</p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="item__img"
+          src="https://cdn.discordapp.com/attachments/1014921169595531369/1018910714322235402/myAvatar.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-        <div className="d-block w-100">
-          <h1>Nombre</h1>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        <div className="text__content">
+          <h1>Gonzalo</h1>
+          <p>Comida favorita: </p>
+          <p>Serie que veríamos 1000 veces: </p>
+          <p>Un lugar en el mundo: Roma</p>
+          <p>Canción que cantas en la ducha: </p>
+          <p>Pizza con o sin piña: </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
@@ -60,9 +74,13 @@ function ControlledCarousel() {
         />
 
         <Carousel.Caption>
-        <div className="d-block w-100">
-          <h1>Nombre</h1>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        <div className="text__content">
+          <h1>Javi</h1>
+          <p>Comida favorita: </p>
+          <p>Serie que veríamos 1000 veces: </p>
+          <p>Un lugar en el mundo: Roma</p>
+          <p>Canción que cantas en la ducha: </p>
+          <p>Pizza con o sin piña: </p>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const About = () => {
   return (
     <div className="cards">
-      <h1>PAGINA SOBRE NOSOTROS</h1>
-      <h1>Aquí ira un poco de información sobre nosotros</h1>
       <ControlledCarousel />
     </div>
   );
